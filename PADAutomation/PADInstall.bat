ddb8a89c491184b537c9d53e8530156176a63024
kodamayuto <yuto2001gyokugan@gmail.com>
2022-04-10 00:07:51 +0900

    修正

--- PADAutomation/PADInstall.bat
+++
@@ -1,7 +0,0 @@
-echo off
-
-if exist "%CD%\Setup.Microsoft.PowerAutomate.exe" (
-    exit /b
-)
-bitsadmin /transfer download https://go.microsoft.com/fwlink/?linkid=2102613 %CD%\Setup.Microsoft.PowerAutomate.exe
-exit /b
--- PADAutomation/StartSetupPAD.bat
+++
@@ -1,9 +0,0 @@
-@powershell -NoProfile -ExecutionPolicy Unrestricted "$s=[scriptblock]::create((gc \"%~f0\"|?{$_.readcount -gt 1})-join\"`n\");&$s" %*&goto:eof
-
-$ps = Get-Process | Where-Object { $_.Name -eq "Setup.Microsoft.PowerAutomate" }
-
-if ($null -eq $ps) {
-    Start-Process Setup.Microsoft.PowerAutomate.exe
-}
-
-.\SetupPAD.bat
